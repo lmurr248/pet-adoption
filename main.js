f415a79b315608b35d6ed531df71b2f468acd362
lmurr248 <55932123+lmurr248@users.noreply.github.com>
2024-04-27 18:31:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,42 @@ async function start() {
 }
 
 start();
+
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-card-age").textContent = createAgeText(
+      pet.birthYear
+    );
+
+    if (!pet.photo) pet.photo = "images/fallback.jpg";
+
+    clone.querySelector(".pet-card-photo img").src = pet.photo;
+    clone.querySelector(
+      ".pet-card-photo img"
+    ).alt = `A ${pet.specis} named ${pet.name}`;
+
+    wrapper.appendChild(clone);
+  });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
+}
+
+petsArea();
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  if (age == 1) return "1 year old";
+  if (age == 0) return "Less than a year old";
+  return `${age} years old`;
+}
